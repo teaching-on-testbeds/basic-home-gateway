4b58e5d36acdd61b1c57f56651f4397f7be13e81
Fraida Fund <ffund@nyu.edu>
2022-10-04 09:07:09 -0400

    Update profile

--- profile.py
+++ profile.py
@@ -1,3 +1,9 @@
+"""This topology has four hosts - three arranged like a home network with a gateway, and a fourth representing an external service. 
+
+To use this topology, follow the instructions at [Basic home gateway services: DHCP, DNS, NAT](https://witestlab.poly.edu/blog/basic-home-gateway-services-dhcp-dns-nat/).
+
+"""
+
 # Import the Portal object.
 import geni.portal as portal
 # Import the ProtoGENI library.
@@ -16,6 +22,8 @@ node_gateway = request.XenVM('gateway')
 node_gateway.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD'
 node_gateway.startVNC()
 node_gateway.routable_control_ip = True
+node_gateway.Site("site1")
+
 iface0 = node_gateway.addInterface('interface-1', pg.IPv4Address('192.168.100.1','255.255.255.0'))
 
 # Node client-1
@@ -23,6 +31,8 @@ node_client_1 = request.XenVM('client-1')
 node_client_1.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD'
 node_client_1.startVNC()
 node_client_1.routable_control_ip = True
+node_client_1.Site("site1")
+
 iface1 = node_client_1.addInterface('interface-2', pg.IPv4Address('0.0.0.0','255.255.255.0'))
 
 # Node client-2
@@ -30,6 +40,9 @@ node_client_2 = request.XenVM('client-2')
 node_client_2.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD'
 node_client_2.startVNC()
 node_client_2.routable_control_ip = True
+
+node_client_2.Site("site1")
+
 iface2 = node_client_2.addInterface('interface-0', pg.IPv4Address('0.0.0.0','255.255.255.0'))
 
 # Node website
@@ -37,6 +50,7 @@ node_website = request.XenVM('website')
 node_website.startVNC()
 node_website.routable_control_ip = True
 node_website.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD'
+node_website.Site("site2")
 
 # Link link-0
 link_0 = request.Link('link-0')
